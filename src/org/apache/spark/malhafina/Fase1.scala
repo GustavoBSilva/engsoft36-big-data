dabb33ed79360c3bb777d772e7de6a16cccd7c9f
Gustavo <gustavobianco.sa@gmail.com>
2020-10-08 19:59:08 -0300

    Fases da aplicacao Spark

---
+++ src/org/apache/spark/malhafina/Fase1.scala
@@ -0,0 +1,145 @@
+package org.apache.spark.malhafina
+
+import java.util.Properties;
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SaveMode
+
+object Fase1 {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setMaster("local").setAppName("Malha Fina")
+
+    val sc = new SparkContext(conf)
+
+    val spark = SparkSession
+      .builder()
+      .appName("Malha Fina")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", "root")
+    connectionProperties.put("password", "123qwe!@#")
+
+    executarFase1(spark, connectionProperties)
+  }
+
+  private def executarFase1(spark: SparkSession, connectionProperties: Properties): Unit = {
+    val path = "/home/gustavo/Projetos/MalhaFina/datasets/"
+
+    val esquemaContribuintes = new StructType()
+      .add("cpf", LongType)
+      .add("nome", StringType)
+      .add("sexo", StringType)
+      .add("data_nascimento", DateType)
+
+    val esquemaRendimentosTributaveis = new StructType()
+      .add("ano_calendario", IntegerType)
+      .add("cnpj", LongType)
+      .add("cpf", LongType)
+      .add("rendimentos", DoubleType)
+      .add("decimo_terceiro", DoubleType)
+
+    val beneficiosDF = spark.read.option("header", true).schema(esquemaRendimentosTributaveis).csv(path + "pessoas-juridicas/beneficios.csv")
+
+    val contribuintesDF = spark.read.option("header", true).schema(esquemaContribuintes).csv(path + "contribuintes.csv")
+    val beneficiariosDF = spark.read.option("header", true).schema(esquemaRendimentosTributaveis).csv(path + "contribuintes/rendimentos_tribut_receb_pj.csv")
+
+    // Save Data to JDBC
+    writeJdbcContribuintes(spark, contribuintesDF, connectionProperties)
+    writeJdbcBeneficios(spark, beneficiosDF, connectionProperties)
+    writeJdbcRendimentosTributaveisPj(spark, beneficiariosDF, connectionProperties)
+
+    beneficiosDF.createOrReplaceTempView("rendimentos_declarados_por_pjs")
+    beneficiariosDF.createOrReplaceTempView("rendimentos_declarados_por_contribuintes")
+    contribuintesDF.createOrReplaceTempView("contribuintes")
+
+    val beneficiosAnoCalendarioDF = spark.sql("select cpf, sum(rendimentos) as total_rendimentos from rendimentos_declarados_por_pjs where ano_calendario = 2019 group by cpf")
+
+    val beneficiariosAnoCalendarioDF = spark.sql("select cpf, sum(rendimentos) as total_rendimentos from rendimentos_declarados_por_contribuintes where ano_calendario = 2019 group by cpf")
+
+    beneficiosAnoCalendarioDF.createOrReplaceTempView("total_rendimentos_declarados_por_pjs")
+    beneficiariosAnoCalendarioDF.createOrReplaceTempView("total_rendimentos_declarados_por_contribuintes")
+
+    val dadosNaoDeclaradosDF = spark.sql("SELECT pj.cpf, pj.total_rendimentos FROM total_rendimentos_declarados_por_pjs pj"
+      + "WHERE pj.cpf NOT IN (SELECT cpf FROM total_rendimentos_declarados_por_contribuintes)")
+
+    val dadosDivergentesDF = spark.sql("SELECT pj.cpf, pj.total_rendimentos total_rendimentos_pj, ct.total_rendimentos total_rendimentos_contrib FROM total_rendimentos_declarados_por_pjs pj"
+      + " JOIN total_rendimentos_declarados_por_contribuintes ct ON ct.cpf = pj.cpf WHERE pj.total_rendimentos <> ct.total_rendimentos")
+
+    writeJdbc(spark, dadosNaoDeclaradosDF, "malhafina.nao_declarados", connectionProperties)
+    writeJdbc(spark, dadosDivergentesDF, "malhafina.divergentes", connectionProperties)
+  }
+
+  private def executarFase2(spark: SparkSession, connectionProperties: Properties): Unit = {
+    //writeJdbcWithoutOptions(spark, dataframe, SaveMode.Overwrite, "schema.tabela", connectionProperties)
+
+  }
+
+  private def writeJdbcContribuintes(spark: SparkSession, dataFrame: DataFrame, connectionProperties: Properties): Unit = {
+
+    // Specifying create table column data types on write
+    dataFrame.write
+      .option("createTableColumnTypes", "data_nascimento DATE")
+      .mode("overwrite")
+      .jdbc("jdbc:mysql://localhost:3306", "malhafina.contribuintes", connectionProperties)
+
+    // Specifying the custom data types of the read schema
+    // The custom schema to use for READING data from JDBC connectors
+    // connectionProperties.put("customSchema", "id BIGINT, cadastrado_em TIMESTAMP")
+
+    // Loading data from a JDBC source
+    // val jdbcDF = spark.read.jdbc("jdbc:mysql://localhost:3306", "malhafina.contribuintes", connectionProperties)
+
+    // jdbcDF.show
+
+    // Another way to save data to a JDBC source
+    /*dataFrame.write.mode(SaveMode.Overwrite)
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306")
+      .option("dbtable", "malhafina.contribuintes")
+      .option("user", "root")
+      .option("password", "123qwe!@#")
+      .save()*/
+  }
+
+  /**
+   * Dados enviados pelos Contribuintes
+   */
+  private def writeJdbcRendimentosTributaveisPj(spark: SparkSession, dataFrame: DataFrame, connectionProperties: Properties): Unit = {
+
+    dataFrame.write
+      .option("createTableColumnTypes", "cnpj VARCHAR(20), cpf VARCHAR(20), rendimentos DECIMAL(35,2)")
+      .mode(SaveMode.Overwrite)
+      .jdbc("jdbc:mysql://localhost:3306", "malhafina.rendimentos_tributaveis_recebidos_pj", connectionProperties)
+  }
+
+  /**
+   * Dados enviados pelas Pessoas Jurídicas
+   */
+  private def writeJdbcBeneficios(spark: SparkSession, dataFrame: DataFrame, connectionProperties: Properties): Unit = {
+
+    dataFrame.write
+      .option("createTableColumnTypes", "cnpj VARCHAR(20), cpf VARCHAR(20), rendimentos DECIMAL(35,2)")
+      .mode(SaveMode.Overwrite)
+      .jdbc("jdbc:mysql://localhost:3306", "malhafina.beneficios", connectionProperties)
+  }
+
+  private def writeJdbc(spark: SparkSession, dataFrame: DataFrame, tableName: String, connectionProperties: Properties): Unit = {
+
+    dataFrame.write
+      .option("createTableColumnTypes", "cpf VARCHAR(20), rendimentos DECIMAL(35,2)")
+      .mode(SaveMode.Overwrite)
+      .jdbc("jdbc:mysql://localhost:3306", tableName, connectionProperties)
+  }
+  
+  private def writeJdbcWithoutOptions(spark: SparkSession, dataFrame: DataFrame, saveMode: SaveMode, tableName: String, connectionProperties: Properties): Unit = {
+    dataFrame.write.mode(saveMode).jdbc("jdbc:mysql://localhost:3306", tableName, connectionProperties)
+  }
+
+}
---
+++ src/org/apache/spark/malhafina/Fase2.scala
@@ -0,0 +1,107 @@
+package org.apache.spark.malhafina
+
+import java.util.Properties;
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.Encoder
+
+import org.apache.spark.sql.Row
+
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SaveMode
+
+object Fase2 {
+  def main(args: Array[String]): Unit = {
+    var appName = "Malha Fina - Fase 2"
+
+    val conf = new SparkConf().setMaster("local").setAppName(appName)
+
+    val sc = new SparkContext(conf)
+
+    val spark = SparkSession
+      .builder()
+      .appName(appName)
+      .getOrCreate()
+
+    val connectionProperties = new Properties()
+    connectionProperties.put("user", "root")
+    connectionProperties.put("password", "123qwe!@#")
+
+    executar(spark, connectionProperties)
+  }
+
+  private def executar(spark: SparkSession, connectionProperties: Properties): Unit = {
+    val path = "/home/gustavo/Projetos/MalhaFina/datasets/"
+
+    val esquemaContas = new StructType()
+      .add("ano_calendario", IntegerType)
+      .add("cpf", StringType)
+      .add("agencia_sem_dv", StringType)
+      .add("conta_num", StringType)
+      .add("conta_dv", StringType)
+      .add("situacao_anterior", DoubleType)
+      .add("situacao_vigente", DoubleType)
+
+    // RDD a partir do arquivo enviado pelos Bancos
+    val rddContas = spark.sparkContext.textFile(path + "instituicoes-financeiras/contas.csv")
+    // RDD a partir do arquivo enviado pelos Contribuintes
+    val rddBensDireitosContribuintes = spark.sparkContext.textFile(path + "contribuintes/contas.csv")
+
+    // DataFrame a partir do arquivo enviado pelos Bancos
+    val contasDF = spark.read.option("header", true).schema(esquemaContas).csv(path + "instituicoes-financeiras/contas.csv")
+    // DataFrame a partir do arquivo enviado pelos Contribuintes
+    val contasContribuintesDF = spark.read.option("header", true).schema(esquemaContas).csv(path + "contribuintes/contas.csv")
+
+    // Salvar dados usando JDBC
+    writeJdbc(spark, contasDF, "contas", connectionProperties)
+    writeJdbc(spark, contasContribuintesDF, "contas_contribuintes", connectionProperties)
+
+    import spark.implicits._
+
+    case class Conta(
+      ano_calendario:    Int,
+      cpf:               String,
+      agencia_sem_dv:    String,
+      conta_num:         String,
+      conta_dv:          String,
+      situacao_anterior: Double,
+      situacao_vigente:  Double)
+
+    contasDF.filter($"ano_calendario" === 2019).select($"cpf", $"situacao_vigente", $"situacao_anterior").createOrReplaceTempView("saldo_contas")
+
+    val saldos = spark.sql("select cpf, situacao_vigente - situacao_anterior as saldo from saldo_contas")
+      .createOrReplaceTempView("total_saldo_contas")
+    val totalSaldos = spark.sql("select cpf, sum(saldo) from total_saldo_contas group by cpf")
+
+    totalSaldos.show
+    
+    contasContribuintesDF.filter($"ano_calendario" === 2019).select($"cpf", $"situacao_vigente", $"situacao_anterior").createOrReplaceTempView("saldo_contas_contribuintes")
+    
+    val saldosContribuintes = spark.sql("select cpf, situacao_vigente - situacao_anterior as saldo from saldo_contas_contribuintes")
+      .createOrReplaceTempView("total_saldo_contas_contribuintes")
+    val totalSaldosContribuintes = spark.sql("select cpf, sum(saldo) from total_saldo_contas_contribuintes group by cpf")
+    
+    totalSaldosContribuintes.show
+
+  }
+
+  private def writeJdbc(spark: SparkSession, dataFrame: DataFrame, tableName: String, connectionProperties: Properties): Unit = {
+
+    val columnTypes = "cpf VARCHAR(20), situacao_anterior DECIMAL(35,2), situacao_vigente DECIMAL(35,2)"
+
+    dataFrame.write
+      .option("createTableColumnTypes", columnTypes)
+      .mode(SaveMode.Overwrite)
+      .jdbc("jdbc:mysql://localhost:3306/malhafina", tableName, connectionProperties)
+  }
+
+  private def writeJdbcWithoutOptions(spark: SparkSession, dataFrame: DataFrame, saveMode: SaveMode, tableName: String, connectionProperties: Properties): Unit = {
+    dataFrame.write.mode(saveMode).jdbc("jdbc:mysql://localhost:3306/malhafina", tableName, connectionProperties)
+  }
+
+}
